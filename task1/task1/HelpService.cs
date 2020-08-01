db16cc89fcbd814f59fde61c1dd2eb319e5de54f
karl-neu <56051588+karl-neu@users.noreply.github.com>
2020-07-31 17:53:25 -0700

    add console app to task1 branch

---
+++ task1/task1/HelpService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    public class HelpService
+    {
+        private string FilePath { get; set; }
+        private string OriginalText { get; set; }
+
+        public HelpService()
+        {
+            var path = Environment.CurrentDirectory + "\\text.txt";
+            if (File.Exists(path)) FilePath = path;
+        }
+
+        public async Task ReadTextFile()
+        {
+            try
+            {
+                using var sr = new StreamReader(FilePath);
+                OriginalText = await sr.ReadToEndAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public async Task WriteToFile()
+        {
+            try
+            {
+                using StreamWriter outputFile = new StreamWriter(FilePath);
+                await outputFile.WriteAsync(OriginalText);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be writed:");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public void DeleteSubstr()
+        {
+            Console.Write("Enter Char/String for delete: ");
+            var str = Console.ReadLine();
+
+            int index = OriginalText.IndexOf(str);
+
+            if (index >= 0)
+            {
+                OriginalText = OriginalText.Replace(str, "");
+                Console.WriteLine($"Deleted!");
+            }
+
+            else Console.WriteLine($"Char/String \"{str}\" not found!");
+        }
+
+        public void ShowCountWords()
+        {
+            string[] words = OriginalText.Split(new char[] { '.', '?', '!', ' ', ';', ':', ',', '\\', '\r', '\n', ')', '(', '\"' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (words != null && words.Length > 0)
+            {
+                Console.WriteLine($"Count of words: {words.Length}");
+
+                Console.WriteLine("Every 10th word: ");
+                for (int i = 9; i < words.Length; i += 10)
+                {
+                    Console.Write(words[i] + ", ");
+                }
+            }
+            else Console.WriteLine("File empty!");
+
+        }
+
+        public void ShowThirdReverseSentence()
+        {
+            string[] sentences = OriginalText.Split(new char[] { '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+            if (sentences.Length > 2)
+                Console.WriteLine(ReverseStr(sentences[2]));
+            else Console.WriteLine("Second sentence not found!");
+        }
+
+        public string ReverseStr(string str)
+        {
+            return string.Join(" ", str.Split(' ').Select(x => new String(x.Reverse().ToArray())));
+        }
+    }
+}
---
+++ task1/task1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("\t\tTask #1");
+            string key;
+
+            HelpService hs = new HelpService();
+            FolderExplorer fe = new FolderExplorer();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\tMethods:");
+                Console.WriteLine(" 1. Read text file and delete special char/string.");
+                Console.WriteLine(" 2. Read text file and show count of words and every 10th word.");
+                Console.WriteLine(" 3. Show 3d sentence with reverse words.");
+                Console.WriteLine(" 4. Show file explorer.");
+                Console.WriteLine(" 0. Exit.");
+
+                key = Console.ReadLine();
+                switch (key)
+                {
+                    case "1":
+                        await hs.ReadTextFile();
+                        hs.DeleteSubstr();
+                        await hs.WriteToFile();
+                        break;
+                    case "2":
+                        await hs.ReadTextFile();
+                        hs.ShowCountWords();
+                        break;
+                    case "3":
+                        await hs.ReadTextFile();
+                        hs.ShowThirdReverseSentence();
+                        break;
+                    case "4":
+                        fe.SetPath();
+                        fe.WalkingTree();
+                        break;
+                    case "0":
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Oops!");
+                        break;
+                }
+                Console.ReadLine();
+            } while (key!="0");
+        }
+    }
+}
